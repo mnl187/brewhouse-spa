6316d4398dfb963d57f41e525f18e581ee7112c8
mnl187 <mnl187@gmail.com>
2023-03-12 23:59:30 +0100

    dodanie wyświetlania komponentu RecpieDetails na stronie

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,5 +1,6 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {Container, Heading, Text } from "@chakra-ui/react";
 
 export const RecipeDetails = () => {
 
@@ -7,7 +8,7 @@ export const RecipeDetails = () => {
     const [recipe, setRecipe] = useState(null);
 
     useEffect(() => {
-        const fetchRecipe = async () => {
+        const fetchRecipe = async (id) => {
             try {
                 const response = await fetch(`/api/recipes/${id}`);
                 const data = await response.json();
@@ -23,5 +24,34 @@ export const RecipeDetails = () => {
     if (!recipe) {
         return <div>Wczytywanie...</div>;
     }
-
+return (
+    <Container maxW="container.lg">
+        {recipe ? (
+            <>
+                <Heading as="h2" mb="4">
+                    {recipe.title}
+                </Heading>
+                <Text mb="4">{recipe.description}</Text>
+                <Text fontWeight="bold" mb="2">
+                    Składniki:
+                </Text>
+                <ul>
+                    {recipe.ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                    ))}
+                </ul>
+                <Text fontWeight="bold" mb="2" mt="4">
+                    Instrukcja:
+                </Text>
+                <ol>
+                    {recipe.instructions.map((step, index) => (
+                        <li key={index}>{step}</li>
+                    ))}
+                </ol>
+            </>
+        ) : (
+            <Text>Wczytywanie...</Text>
+        )}
+    </Container>
+);
 }
