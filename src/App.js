84651d697c5080bf80a7a6b708570454d3da9ee2
mnl187 <mnl187@gmail.com>
2023-03-14 21:21:20 +0100

    dodanie komponentu do App.js, dodano także Navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import {Home} from "./components/Home";
 import {ChakraProvider} from "@chakra-ui/react";
 import {Header} from "./components/Header";
@@ -8,6 +8,7 @@ import {NotFound} from "./components/NotFound";
 import {RecipeDetails} from "./components/RecipeDetails";
 import {AddRecipe} from "./components/AddRecipe";
 import {EditRecipeForm} from "./components/EditRecipeForm";
+import {DeleteRecipe} from "./components/DeleteRecipe";
 
 export const App = () => {
     return (
@@ -19,8 +20,10 @@ export const App = () => {
                     <Route path="/recipe" element={<Recipe/>}/>
                     <Route path="/recipe/{id}" element={<RecipeDetails/>}/>
                     <Route path="/recipe/:id/edit" element={<EditRecipeForm />}/>
+                    <Route path="/recipes/:id/delete" element={<DeleteRecipe/>}/>
                     <Route path="/add-recipe" element={<AddRecipe/>}/>
                     <Route path="*" element={<NotFound/>}/>
+                    <Navigate to="/" />
                 </Routes>
             </Router>
         </ChakraProvider>
