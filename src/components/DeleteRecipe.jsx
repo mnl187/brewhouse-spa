ed738da81a2df6d6523a0b326c876b1322a33642
mnl187 <mnl187@gmail.com>
2023-03-14 21:10:30 +0100

    dodanie obsługi przycisków Usuń i Anuluj korzystając z hooka useNavigate

--- src/components/DeleteRecipe.jsx
+++ src/components/DeleteRecipe.jsx
@@ -1,18 +1,21 @@
 import {Heading, VStack, Text, Button} from "@chakra-ui/react";
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 
-export const DeleteRecipe = () => {
-
+export const DeleteRecipe = (recipe, onDelete) => {
+    const navigate = useNavigate();
     const handleDelete = () => {
-
+        onDelete(recipe.id);
+        navigate('/')
     }
 
     return (
         <VStack>
             <Heading>Usuwanie przepisu</Heading>
             <Text>Czy jesteś pewny, ze chcesz usunąć przepis?</Text>
-            <Button onClick={handleDelete}></Button>
+            <Button onClick={handleDelete}>Usuń</Button>
+            <Button onClick={() => navigate('/')}>Anuluj</Button>
         </VStack>
 
     );
