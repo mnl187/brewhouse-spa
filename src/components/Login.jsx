ac4be1d463966dbdfa9efbcd254cc8736be18ecc
mnl187 <mnl187@gmail.com>
2023-03-14 21:51:19 +0100

    błędów związanych z importem Buton zamiast Button :) dodanie logiki logowania

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,25 @@
 import React, {useState} from "react";
-import {Box, FormControl, FormLabel, Input} from "@chakra-ui/react";
+import {Box, Button, FormControl, FormLabel, Input} from "@chakra-ui/react";
 import {useNavigate} from "react-router-dom";
-import * as PropTypes from "prop-types";
 
-function Buton(props) {
-    return null;
-}
-
-Buton.propTypes = {children: PropTypes.node};
 export const Login = () => {
     const navigate = useNavigate()
     const [username, setUsername] = useState("");
 
+    const handeSubmit = (e) => {
+        e.preventDefault();
+        navigate("/");
+    }
 
     return (
         <Box>
-            <form action="">
+            <form onSubmit={handeSubmit}>
                 <FormControl>
                     <FormLabel htmlFor="username">Nazwa użytkownika</FormLabel>
                     <Input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
                 </FormControl>
-                <Buton type="submit">Zaloguj</Buton>
-                <Buton onClick={() => navigate("/")}>Anuluj</Buton>
+                <Button type="submit">Zaloguj</Button>
+                <Button onClick={() => navigate("/")}>Anuluj</Button>
             </form>
         </Box>
     )
