0bdc7211862bd62101bf2a92a6cbc5908bcd9a67
mnl187 <mnl187@gmail.com>
2023-03-14 21:38:40 +0100

    dodanie obsługi buttonów

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,13 @@
 import React, {useState} from "react";
 import {Box, FormControl, FormLabel, Input} from "@chakra-ui/react";
 import {useNavigate} from "react-router-dom";
+import * as PropTypes from "prop-types";
 
+function Buton(props) {
+    return null;
+}
+
+Buton.propTypes = {children: PropTypes.node};
 export const Login = () => {
     const navigate = useNavigate()
     const [username, setUsername] = useState("");
@@ -14,8 +20,8 @@ export const Login = () => {
                     <FormLabel htmlFor="username">Nazwa użytkownika</FormLabel>
                     <Input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
                 </FormControl>
-                <Buton>Zaloguj</Buton>
-                <Buton>Anuluj</Buton>
+                <Buton type="submit">Zaloguj</Buton>
+                <Buton onClick={() => navigate("/")}>Anuluj</Buton>
             </form>
         </Box>
     )
