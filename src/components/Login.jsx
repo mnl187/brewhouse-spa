57dc281f5474918ffb4302d3ee856e66ca7bb2e4
mnl187 <mnl187@gmail.com>
2023-03-14 22:23:10 +0100

    poprawa kodu rezygnując z nowego pomysłu logowania poprzez email, powrót do username

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,14 +6,13 @@ export const Login = (onLogin) => {
     const navigate = useNavigate()
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
     const [error, setError] = useState(null);
 
     const handeSubmit = (e) => {
         e.preventDefault();
         // walidacja do przerobienia jak powstanie backend
-        if (email === 'user@site.com' && password === 'password') {
-            onLogin(email);
+        if (username === 'username' && password === 'password') {
+            onLogin(username);
             navigate("/");
         } else {
             setError('Niepoprawny email lub hasło');
@@ -21,7 +20,7 @@ export const Login = (onLogin) => {
     };
 
     const handleCancel = () => {
-        setEmail('');
+        setUsername('');
         setPassword('');
         setError(null);
     }
