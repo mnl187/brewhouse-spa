3beb5666f966d003766511424862653a5639239d
mnl187 <mnl187@gmail.com>
2023-03-14 21:45:25 +0100

    dodanie routingu strony logowania

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {Home} from "./components/Home";
 import {ChakraProvider} from "@chakra-ui/react";
 import {Header} from "./components/Header";
@@ -9,6 +9,7 @@ import {RecipeDetails} from "./components/RecipeDetails";
 import {AddRecipe} from "./components/AddRecipe";
 import {EditRecipeForm} from "./components/EditRecipeForm";
 import {DeleteRecipe} from "./components/DeleteRecipe";
+import {Login} from "./components/Login";
 
 export const App = () => {
     return (
@@ -22,8 +23,8 @@ export const App = () => {
                     <Route path="/recipe/:id/edit" element={<EditRecipeForm />}/>
                     <Route path="/recipes/:id/delete" element={<DeleteRecipe/>}/>
                     <Route path="/add-recipe" element={<AddRecipe/>}/>
+                    <Route path="/login" element={<Login/>}/>
                     <Route path="*" element={<NotFound/>}/>
-                    <Navigate to="/" />
                 </Routes>
             </Router>
         </ChakraProvider>
