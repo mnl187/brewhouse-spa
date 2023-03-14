659c1968b6cae436d8b4516855e5c298aa3dbdc9
mnl187 <mnl187@gmail.com>
2023-03-14 21:36:37 +0100

    dodanie obługi Input username, dodanie 2 buttonów

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import {Box, FormControl, FormLabel, Input} from "@chakra-ui/react";
 import {useNavigate} from "react-router-dom";
 
 export const Login = () => {
+    const navigate = useNavigate()
     const [username, setUsername] = useState("");
 
 
@@ -11,8 +12,10 @@ export const Login = () => {
             <form action="">
                 <FormControl>
                     <FormLabel htmlFor="username">Nazwa użytkownika</FormLabel>
-                    <Input type="text" id="username" value={username}></Input>
+                    <Input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
                 </FormControl>
+                <Buton>Zaloguj</Buton>
+                <Buton>Anuluj</Buton>
             </form>
         </Box>
     )
