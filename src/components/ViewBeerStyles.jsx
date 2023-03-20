b5cf9115168b49134142e9260be81b16ab34f026
mnl187 <mnl187@gmail.com>
2023-03-20 06:58:40 +0100

    dodanie metody fetch do pobierania danych z json, w przyszłości będzie zamieniona na pobieranie danych z backendu

--- src/components/ViewBeerStyles.jsx
+++ src/components/ViewBeerStyles.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export const ViewBeerStyles = () => {
 
+    const [beerStyles, setBeerStyles] = useState([]);
+
+    useEffect(() => {
+        fetch('../api/recipes')   // TODO: później zmienić na endopint z backendu
+            .then((response) => response.json())
+            .then((data) => setBeerStyles(data))
+    },[])
     return (
         <ul>
             <li>style name</li>
