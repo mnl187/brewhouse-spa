a498dd4b09d54dbcab0ccc8f1b06aff4bdc9bb48
mnl187 <mnl187@gmail.com>
2023-03-23 22:28:09 +0100

    dodanie komponentu ExtrasForm

---
+++ src/components/ExtrasForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {IngredientForm} from "../pages/IngredientForm";
+import {Button, VStack} from "@chakra-ui/react";
+
+export const ExtrasForm = ({extras, setExtras}) => {
+    return (
+        <VStack>
+            <IngredientForm
+                label="Dodatki"
+                ingredients={extras}
+                setIngredients={setExtras}
+                index={0}
+            />
+            {extras.slice(1).map((_, index) => (
+                <IngredientForm
+                    key={index + 1}
+                    label={`Dodatki ${index + 2}`}
+                    ingredients={extras}
+                    setIngredients={setExtras}
+                    index={index + 1}
+                />
+            ))}
+            <Button onClick={() => setExtras([...extras, { name: '', amount: '' }])}>
+                Dodaj dodatki
+            </Button>
+        </VStack>
+    )
+}
