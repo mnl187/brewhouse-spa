bd5a4a0c454783c28e3a93c6a7ea563f6523d23d
mnl187 <mnl187@gmail.com>
2023-03-23 22:23:48 +0100

    dodanie komponentu HopsForm (analogiczny do MaltsForm)

---
+++ src/components/HopsForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {IngredientForm} from "../pages/IngredientForm";
+import {Button, VStack} from "@chakra-ui/react";
+
+export const ChopsForm = ({hops, setHops}) => {
+    return (
+        <VStack>
+            <IngredientForm
+                label="Chmiel"
+                ingredients={hops}
+                setIngredients={setHops}
+                index={0}
+            />
+            {hops.slice(1).map((_, index) => (
+                <IngredientForm
+                    key={index + 1}
+                    label={`Chmiel ${index + 2}`}
+                    ingredients={hops}
+                    setIngredients={setHops}
+                    index={index + 1}
+                />
+            ))}
+            <Button onClick={() => setHops([...hops, { name: '', amount: '' }])}>
+                Dodaj chmiel
+            </Button>
+        </VStack>
+    )
+}
