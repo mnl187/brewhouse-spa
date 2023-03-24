a07fb63b613c835fcdc9aa2a6796b67f78ac4727
mnl187 <mnl187@gmail.com>
2023-03-24 07:33:25 +0100

    dodanie hooka useEffect, który wewnątrz fetch wykonuje żadanie do endpointu API

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Box, Grid, GridItem, Heading, Text} from '@chakra-ui/react';
 
 export const Recipes = () => {
     const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/beers')
+            .then((response) => response.json())
+            .then((data) => setRecipes(data));
+    }, []);
 
     return (
         <Box maxW="600px" mx="auto" mt="8">
