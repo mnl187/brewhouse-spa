f7a5597f58a79e365c1b971d4d4305b5b83b6a11
mnl187 <mnl187@gmail.com>
2023-03-30 18:28:09 +0200

    dodanie do funkcji  handleSubmit żadania aktualizującego kod w db za pomocą metody PUT, pooprawa kodu EditRecpieForm

--- src/components/EditRecipeForm.jsx
+++ src/components/EditRecipeForm.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { Box, Button, FormControl, FormLabel, Input, Heading } from "@chakra-ui/react";
+import React, {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {Box, Button, FormControl, FormLabel, Input, Heading} from "@chakra-ui/react";
 
-export const EditRecipeForm = ({ onUpdate }) => {
+export const EditRecipeForm = ({onUpdate}) => {
     const [recipe, setRecipe] = useState(null);
-    const { id } = useParams();
+    const {id} = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -17,106 +17,119 @@ export const EditRecipeForm = ({ onUpdate }) => {
     }, [id]);
 
     if (!recipe) {
-        return <p>Loading...</p>;
+        return <p>Wczytywanie...</p>;
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        onUpdate(id, {
-            name: recipe.name,
-            selectedStyle: recipe.selectedStyle,
-            malts: recipe.malts,
-            hops: recipe.hops,
-            yeast: recipe.yeast,
-            extras: recipe.extras,
+
+        const response = await fetch(`http://localhost:5000/beers/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+
+            body: JSON.stringify({
+                name: recipe.name,
+                selectedStyle: recipe.selectedStyle,
+                malts: recipe.malts,
+                hops: recipe.hops,
+                yeast: recipe.yeast,
+                extras: recipe.extras,
+            }),
         });
-        navigate(-1);
-    };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setRecipe((prevState) => ({ ...prevState, [name]: value }));
-    };
+        const handleChange = (e) => {
+            const {name, value} = e.target;
+            setRecipe((prevState) => ({...prevState, [name]: value}));
+        };
 
-    return (
-        <Box maxW="600px" mx="auto" mt="8">
-            <Heading as="h2" size="lg" mb="4">
-                Edytuj przepis
-            </Heading>
-            <form onSubmit={handleSubmit}>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="name">Styl piwny</FormLabel>
-                    <Input
-                        w="25vw"
-                        type="text"
-                        id="selectedStyle"
-                        name="selectedStyle"
-                        value={recipe.selectedStyle}
-                        isReadOnly
-                        bg="gray.100"
-                    />
-                </FormControl>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="name">Nazwa</FormLabel>
-                    <Input
-                        w="25vw"
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={recipe.name}
-                        onChange={handleChange}
-                    />
-                </FormControl>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="name">Słód</FormLabel>
-                    <Input
-                        w="25vw"
-                        type="text"
-                        id="malts"
-                        name="malts"
-                        value={recipe.malts}
-                        onChange={handleChange}
-                    />
-                </FormControl>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="name">Chmiel</FormLabel>
-                    <Input
-                        w="25vw"
-                        type="text"
-                        id="hops"
-                        name="hops"
-                        value={recipe.hops}
-                        onChange={handleChange}
-                    />
-                </FormControl>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="name">Drożdże</FormLabel>
-                    <Input
-                        w="25vw"
-                        type="text"
-                        id="yeast"
-                        name="yeast"
-                        value={recipe.yeast}
-                        onChange={handleChange}
-                    />
-                </FormControl>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="name">Dodatki</FormLabel>
-                    <Input
-                        w="25vw"
-                        type="text"
-                        id="yeast"
-                        name="v"
-                        value={recipe.yeast}
-                        onChange={handleChange}
-                    />
-                </FormControl>
+        const formatValue = (array) => {
+            return array.map((item) => `${item.name}: ${item.amount}`).join(', ');
+        };
 
-                <Button type="submit" colorScheme="teal" mr="4">
-                    Zapisz
-                </Button>
-                <Button onClick={() => navigate(-1)}>Anuluj</Button>
-            </form>
-        </Box>
-    );
+
+        return (
+            <Box maxW="600px" mx="auto" mt="8">
+                <Heading as="h2" size="lg" mb="4">
+                    Edytuj przepis
+                </Heading>
+                <form onSubmit={handleSubmit}>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="selectedStyle">Styl piwny</FormLabel>
+                        <Input
+                            w="25vw"
+                            type="text"
+                            id="selectedStyle"
+                            name="selectedStyle"
+                            value={recipe.selectedStyle}
+                            isReadOnly
+                            bg="gray.100"
+                        />
+                    </FormControl>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="name">Nazwa</FormLabel>
+                        <Input
+                            w="25vw"
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={recipe.name}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="malts">Słód</FormLabel>
+                        <Input
+                            w="25vw"
+                            type="text"
+                            id="malts"
+                            name="malts"
+                            value={formatValue(recipe.malts.name)}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="hops">Chmiel</FormLabel>
+                        <Input
+                            w="25vw"
+                            type="text"
+                            id="hops"
+                            name="hops"
+                            value={recipe.hops}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="yeast">Drożdże</FormLabel>
+                        <Input
+                            w="25vw"
+                            type="text"
+                            id="yeast"
+                            name="yeast"
+                            value={recipe.yeast}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="extras">Dodatki</FormLabel>
+                        <Input
+                            w="25vw"
+                            type="text"
+                            id="extras"
+                            name="extras"
+                            value={recipe.extras}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
+
+                    <Button type="submit" colorScheme="teal" mr="4">
+                        Zapisz
+                    </Button>
+                    <Button onClick={() => navigate(-1)}>Anuluj</Button>
+                </form>
+            </Box>
+        );
+    }
 }
