f9ddcc0a32957b7145a11c8cf8ae7a24403082dd
mnl187 <mnl187@gmail.com>
2023-04-04 21:10:15 +0200

    dodanie Grid do BeerStylesDetailsPage

--- src/pages/BeerStyleDetailsPage.jsx
+++ src/pages/BeerStyleDetailsPage.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Box, Heading, Text} from "@chakra-ui/react";
+import {Grid, GridItem, Heading, Text} from "@chakra-ui/react";
 import {useParams} from 'react-router-dom';
 
 export const BeerStyleDetailsPage = () => {
@@ -17,16 +17,36 @@ export const BeerStyleDetailsPage = () => {
     }
 
     return (
-        <Box>
-            <Heading as="h2" size="lg" mb="4">
-                Nazwa: {beerStyleDetails.name}
-            </Heading>
-            <Text>
-                Numer: {beerStyleDetails.number}
-            </Text>
-            <Text>
-                Kategoria: {beerStyleDetails.category}
-            </Text>
-        </Box>
+        <Grid templateRows='repeat(3, 1fr)' gap={2}>
+            <GridItem colSpan={1} mx="auto" pt="8" colEnd="auto" justifySelf="end">
+                <Heading as="h2" size="lg" mb="4">
+                    nazwa: {beerStyleDetails.name}
+                </Heading>
+            </GridItem>
+            <GridItem colSpan={1} mx="auto" colEnd="auto" justifySelf="end">
+                <Text>
+                    <strong>Kategoria:</strong> {beerStyleDetails.category}
+                </Text>
+                <Text as="span"> ABV: {beerStyleDetails.abvmin}% - {beerStyleDetails.abvmax}%</Text>
+                <Text as="span"> IBU: {beerStyleDetails.ibumin} - {beerStyleDetails.ibumax}</Text>
+                <Text as="span"> OG: {beerStyleDetails.ogmin} - {beerStyleDetails.ogmax}, </Text>
+                <Text as="span"> FG: {beerStyleDetails.fgmin} - {beerStyleDetails.fgmax}, </Text>
+                <Text as="span"> Barwa: {beerStyleDetails.srmmin} - {beerStyleDetails.srmmax}</Text>
+            </GridItem>
+            <GridItem colSpan={1} mx="auto" colEnd="auto" justifySelf="end">
+                <Text>
+                    <strong>Wygląd:</strong> {beerStyleDetails.appearance}
+                </Text>
+                <Text>
+                    <strong>Smak:</strong> {beerStyleDetails.flavor}
+                </Text>
+                <Text>
+                    <strong>Składniki:</strong> {beerStyleDetails.characteristicingredients}
+                </Text>
+                <Text>
+                    <strong>Instrukcje:</strong> {beerStyleDetails.entryinstructions}
+                </Text>
+            </GridItem>
+        </Grid>
     )
 }
