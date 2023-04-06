326326b68cb2319cef3819e071bf1ba6590b6c19
mnl187 <mnl187@gmail.com>
2023-04-06 06:49:08 +0200

    dodanie obsługi endpoinntu z backend dot. logowania

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
-import { Box, Button, FormControl, FormLabel, Heading, Input, VStack } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import React, {useState} from "react";
+import {Box, Button, FormControl, FormLabel, Heading, Input, VStack} from "@chakra-ui/react";
+import {useNavigate} from "react-router-dom";
 
 export const Login = ({setLoggedIn}) => {
     const navigate = useNavigate()
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+
+    const login = async (username, password) => {
+        try {
+            const response = await fetch('/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({username, password})
+            });
+
+
+        }
+    }
+
 
     const handeSubmit = (e) => {
         e.preventDefault();
@@ -27,26 +42,30 @@ export const Login = ({setLoggedIn}) => {
 
     return (
         <VStack>
-        <Heading>Witam w Brewhouse, aplikacji piwowarskiej.</Heading>
-        <Box maxW="600px" mx="auto" py="8">
-            <form onSubmit={handeSubmit}>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="username">Nazwa użytkownika</FormLabel>
-                    <Input w="25vw" type="text" id="username" value={username}
-                           onChange={(e) => setUsername(e.target.value)} required/>
-                </FormControl>
-                <FormControl mb="4">
-                    <FormLabel htmlFor="password">Hasło</FormLabel>
-                    <Input w="25vw" type="text" id="password" value={password}
-                           onChange={(e) => setPassword(e.target.value)} required/>
-                </FormControl>
+            <Heading>Witam w Brewhouse, aplikacji piwowarskiej.</Heading>
+            <Box maxW="600px" mx="auto" py="8">
+                <form onSubmit={handeSubmit}>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="username">Nazwa użytkownika</FormLabel>
+                        <Input w="25vw" type="text" id="username" value={username}
+                               onChange={(e) => setUsername(e.target.value)} required/>
+                    </FormControl>
+                    <FormControl mb="4">
+                        <FormLabel htmlFor="password">Hasło</FormLabel>
+                        <Input
+                            w="25vw"
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} required/>
+                    </FormControl>
 
-                {error && <Box color="red">{error}</Box>}
+                    {error && <Box color="red">{error}</Box>}
 
-                <Button type="submit" colorScheme="teal" mr="4">Zaloguj</Button>
-                <Button type="button" colorScheme="blue" onClick={handleCancel}>Anuluj</Button>
-            </form>
-        </Box>
+                    <Button type="submit" colorScheme="teal" mr="4">Zaloguj</Button>
+                    <Button type="button" colorScheme="blue" onClick={handleCancel}>Anuluj</Button>
+                </form>
+            </Box>
         </VStack>
     );
 };
